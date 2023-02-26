2c763ac035349c411a14a04d60f46357a0c7d33a
dgjenni2 <digdug14@comcast.net>
2023-02-26 13:09:28 -0600

    Update the angular site with async service call

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,8 @@ export class HeroService {
 
   }
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,7 +12,7 @@ export class HeroesComponent {
 
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
   heroes: Hero[] = [];
   selectedHero?: Hero;
